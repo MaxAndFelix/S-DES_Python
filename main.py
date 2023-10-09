1871e2085ce19eedf8ebc298b0b9550d9ffba905
LocheZ <1913565532@qq.com>
2023-10-09 21:13:28 +0800

    界面预览

---
+++ main.py
@@ -0,0 +1,20 @@
+from PyQt5.QtWidgets import QWidget, QApplication
+from ui_main import Ui_Form
+
+
+class Main(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        # 使用ui文件导入定义界面类
+        self.ui = Ui_Form()
+        # 初始化界面
+        self.ui.setupUi(self)
+
+        # 使用界面定义的控件，也是从ui里面访问
+
+
+app = QApplication([])
+main = Main()
+main.show()
+app.exec_()
